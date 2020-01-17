13cfb2bdb9fbeab5884f8ed576ad570e063d318a
Max Schillinger <maxschillinger@web.de>
2020-01-17 22:13:13 +0100

    improved kou-activate.bash

--- scripts/bin/kou-activate.bash
+++ scripts/bin/kou-activate.bash
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+# get directory of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # activate kou keyboard layout
-xkbcomp kou.xkb $DISPLAY
+xkbcomp -w 0 $DIR/kou.xkb $DISPLAY && echo -e '\nKOU keyboard layout activated.' || exit 1
 
 # reset layout to QWERTZ by typing `asdf<Return>`
 alias haei='setxkbmap de'
+echo 'Type "asdf" (="haei") for setting the QWERTZ layout!'
 
 # add alias for resetting to ~/.bash_aliases
 grep -q "alias haei='setxkbmap de'" ~/.bash_aliases || echo -e "\n# alias added temporarily by schimax\nalias haei='setxkbmap de'" >> ~/.bash_aliases
