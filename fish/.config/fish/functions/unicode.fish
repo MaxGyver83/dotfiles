3cda40f2d21bf52620820a4989dcd5bc70f75d2e
Max Schillinger <maxschillinger@web.de>
2020-04-17 19:32:48 +0200

    fish: improve 'unicode' function: make it work for more than a single character

--- fish/.config/fish/functions/unicode.fish
+++ fish/.config/fish/functions/unicode.fish
@@ -1,3 +1,11 @@
-function unicode --description 'Return Unicode code point for a symbol'
-    echo -n "$argv" | iconv -f utf8 -t utf32be | xxd -p | sed -r 's/^0+/0x/' | xargs printf 'U+%04X\n'
+function unicode --description 'Return Unicode code point for every given symbol'
+    set char_count (string length $argv)
+    for i in (seq $char_count)
+        set char (string sub -s $i -l 1 $argv)
+        # if $argv is more than a single char, print it before its Unicode code point
+        if test $char_count -gt 1
+            echo -n $char': '
+        end
+        echo -n $char | iconv -f utf8 -t utf32be | xxd -p | sed -r 's/^0+/0x/' | xargs printf 'U+%04X\n'
+    end
 end
