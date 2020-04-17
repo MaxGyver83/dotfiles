109e97a0cad8b78be8e712e5ca16ff25e3832908
Max Schillinger <maxschillinger@web.de>
2020-04-17 19:41:37 +0200

    fish: simplify 'unicode' function

--- fish/.config/fish/functions/unicode.fish
+++ fish/.config/fish/functions/unicode.fish
@@ -1,7 +1,6 @@
 function unicode --description 'Return Unicode code point for every given symbol'
     set char_count (string length -- "$argv")
-    for i in (seq $char_count)
-        set char (string sub -s $i -l 1 -- "$argv")
+    for char in (string split '' -- "$argv")
         # if $argv is more than a single char, print it before its Unicode code point
         if test $char_count -gt 1
             echo -n $char': '
