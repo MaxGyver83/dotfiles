4d37f5f6917bddbbc52601b27433d78dc62c9a07
Max Schillinger <maxschillinger@web.de>
2020-05-15 09:22:58 +0200

    fish alias vou: activate koy before setting vou via vou.xkb

--- fish/.config/fish/aliases.fish
+++ fish/.config/fish/aliases.fish
@@ -53,7 +53,7 @@ alias feh='feh --scale-down --auto-zoom --auto-rotate'
 alias batp='bat -pp'
 alias batl='bat --pager="less"'
 
-alias vou='xkbcomp ~/bin/vou.xkb $DISPLAY'
+alias vou='setxkbmap de koy && xkbcomp ~/bin/vou.xkb $DISPLAY'
 
 # git
 abbr g 'git status'
