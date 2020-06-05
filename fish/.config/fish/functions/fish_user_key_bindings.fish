b7de4e8f35f683c32e353a84c972e930ccad9473
Max Schillinger <maxschillinger@web.de>
2020-06-05 21:13:11 +0200

    fish: jump bigword with Ctrl-b/f or Shift-Left/Right

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,8 +1,15 @@
 function fish_user_key_bindings
-    # Ctrl-f completes only next word of autosuggestion
-    bind \cf forward-word
-    # delete bigword (p.e. whole path with Ctrl-w, as in bash)
+    # Ctrl-w: delete bigword (p.e. whole path with Ctrl-w, as in bash)
     bind \cw backward-kill-bigword
+    # Ctrl-f completes only next word of autosuggestion
+    #bind \cf forward-word
+    # jump bigword with Ctrl+b/f
+    bind \cb backward-bigword
+    bind \cf forward-bigword
+    # jump bigword with Shift+LeftArrow/RightArrow
+    bind '[1;2D' backward-bigword
+    bind '[1;2C' forward-bigword
+
     if type -q fzf_key_bindings
         fzf_key_bindings
     end
