0243a27bbbe3b1e1c5074242853ee8cdce92f25d
Max Schillinger <maxschillinger@web.de>
2020-08-03 21:57:37 +0200

    surfingkeys: update config, add Ctrl-. for Ctrl-Tab

--- surfingkeys/.surfingkeys.js
+++ surfingkeys/.surfingkeys.js
@@ -5,14 +5,14 @@ Hints.characters = 'caeiybtrnsoughzx,dwm';
 settings.hintAlign = "right";
 
 settings.blacklistPattern = /groups.google.com/i;
-unmapAllExcept(['f', 'af', 'gf','q', 'i', 'H', 't', 'S', 'D', 'e', 'd', 'se', '?']);
+unmapAllExcept(['f', 'af', 'gf','q', 'i', 'H', 't', 'S', 'D', 'E', 'R', 'e', 'd', ';e', '<Ctrl-6>', '?']);
 //unmap('r'); // Ctrl-r / F5
 //unmap('x'); // Ctrl-w
 
 unmapAllExcept([], /urac.github.io\/Belegungstester/i);
-unmapAllExcept(['f', 'q', 'i', 'cs'], /mail\.google\.com/i);
-unmapAllExcept(['f', 'q', 'i', 'cs'], /maximilian-schillinger\.de\/notizen.html/i);
-unmapAllExcept(['f', 'q', 'i', 'cs', 'T'], /http:\/\/127\.0\.0\.1:8080/i);
+unmapAllExcept(['f', 'q', 'i', ';e'], /mail\.google\.com/i);
+unmapAllExcept(['f', 'q', 'i', ';e', 'T', '<Ctrl-6>'], /maximilian-schillinger\.de\/notizen.html/i);
+unmapAllExcept(['f', 'q', 'i', ';e', 'T', '<Ctrl-6>'], /http:\/\/127\.0\.0\.1:8080/i);
 //unmap('t', /http:\/\/127\.0\.0\.1:8080/i);
 
 map('`', "'"); // use ` instead of '
@@ -22,11 +22,10 @@ vmap(',', ';');
 vmap(';', 'temp');
 vunmap('temp');
 
-map('temp', 'h'); // left
-map('h', 'j');    // down
-map('j', 'l');    // right
+// VOU, top right: L, H, F, J → follow link, left, right, down
+map('temp', 'l'); // right
 map('l', 'f');    // follow link
-map('f', 'temp'); // left
+map('f', 'temp'); // right
 unmap('temp');
 
 map('al', 'af');
@@ -35,7 +34,13 @@ map('cl', 'cf');
 
 map(',', 'd'); // scroll down (up='e')
 
-map('K', 'E'); // tab left
-map('!', 'R'); // tab right
+map('V', 'E'); // tab left
+map('•', 'R'); // tab right
 map('A', 'S'); // back in history
 map('E', 'D'); // forward in history
+
+map('<Ctrl-.>', '<Ctrl-6>'); // go to last active tab
+
+mapkey('p', '#0enter ephemeral PassThrough mode to temporarily suppress SurfingKeys', function() {
+    Normal.passThrough(1500);
+});
