72c88f603bb49bce7d3f10c510eacfb225214243
Max Schillinger <maxschillinger@web.de>
2020-08-09 11:09:21 +0200

    dwm: autostart sxhkd and dunst

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -1,3 +1,6 @@
 feh --bg-scale /home/max/Bilder/Wallpaper/SeaSunset.jpg
-setxkbmap de kou && bash /home/max/bin/vou-activate.bash
+setxkbmap de kou && xkbcomp -w 0 ~/bin/vou-tab.xkb $DISPLAY
 xset r rate 300 50
+#xinput set-prop 'ETPS/2 Elantech Touchpad' 'libinput Natural Scrolling Enabled' 1
+sxhkd &
+dunst &
