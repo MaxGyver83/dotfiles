2501b2c88031481082d5c2fb6350a9a5eb6ef630
Maximilian Schillinger <maximilian.schillinger@daimler.com>
2020-08-10 13:38:57 +0200

    dwm: autostart: deactivate numlock; choose wallpaper depending on system

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -1,6 +1,9 @@
-feh --bg-scale /home/max/Bilder/Wallpaper/SeaSunset.jpg
-setxkbmap de kou && xkbcomp -w 0 ~/bin/vou-tab.xkb $DISPLAY
+test -d /home/max && feh --bg-fill ~/Bilder/Wallpaper/SeaSunset.jpg || feh --bg-fill ~/Pictures/Leo3.jpg
+numlockx off
+setxkbmap de koy && xkbcomp -w 0 ~/bin/vou-tab.xkb $DISPLAY
 xset r rate 300 50
 #xinput set-prop 'ETPS/2 Elantech Touchpad' 'libinput Natural Scrolling Enabled' 1
 sxhkd &
 dunst &
+#st -e fish &
+
