72cb7c6b158079bf7b071e7cc255810d874fc1a4
Maximilian Schillinger <maximilian.schillinger@daimler.com>
2020-08-10 13:44:15 +0200

    st: set gruvbox colorscheme

--- st/repos/st/config.h
+++ st/repos/st/config.h
@@ -94,34 +94,56 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
+/* static const char *colorname[] = { */
+/* 	/1* 8 normal colors *1/ */
+/* 	"black", */
+/* 	"red3", */
+/* 	"green3", */
+/* 	"yellow3", */
+/* 	"blue2", */
+/* 	"magenta3", */
+/* 	"cyan3", */
+/* 	"gray90", */
+
+/* 	/1* 8 bright colors *1/ */
+/* 	"gray50", */
+/* 	"red", */
+/* 	"green", */
+/* 	"yellow", */
+/* 	"#5c5cff", */
+/* 	"magenta", */
+/* 	"cyan", */
+/* 	"white", */
+
+/* 	[255] = 0, */
+
+/* 	/1* more colors can be added after 255 to use with DefaultXX *1/ */
+/* 	"#cccccc", */
+/* 	"#555555", */
+/* }; */
+
+// gruvbox color scheme
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+  /* 8 normal colors */
+  [0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+  [1] = "#cc241d", /* red     */
+  [2] = "#98971a", /* green   */
+  [3] = "#d79921", /* yellow  */
+  [4] = "#458588", /* blue    */
+  [5] = "#b16286", /* magenta */
+  [6] = "#689d6a", /* cyan    */
+  [7] = "#a89984", /* white   */
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
-
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+  /* 8 bright colors */
+  [8]  = "#928374", /* black   */
+  [9]  = "#fb4934", /* red     */
+  [10] = "#b8bb26", /* green   */
+  [11] = "#fabd2f", /* yellow  */
+  [12] = "#83a598", /* blue    */
+  [13] = "#d3869b", /* magenta */
+  [14] = "#8ec07c", /* cyan    */
+  [15] = "#ebdbb2", /* white   */
 };
-
 
 /*
  * Default colors (colorname index)
