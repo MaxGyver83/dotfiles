63725b59bc844b2ed81b2503e20298b7b56a5c63
Max Schillinger <maxschillinger@web.de>
2020-08-10 23:07:01 +0200

    st: change cursor color to white

--- st/repos/st/config.h
+++ st/repos/st/config.h
@@ -151,7 +151,7 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 7;
 unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
+static unsigned int defaultcs = 15;
 static unsigned int defaultrcs = 257;
 
 /*
