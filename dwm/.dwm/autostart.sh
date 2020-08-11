532b1c3126888c388d43971a4ea6937ccaabc1cf
Max Schillinger <maxschillinger@web.de>
2020-08-11 21:40:30 +0200

    dwm: autostart vou layout with Win on Tab key

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -1,6 +1,6 @@
 test -d /home/max && feh --bg-fill ~/Bilder/Wallpaper/SeaSunset.jpg || feh --bg-fill ~/Pictures/Leo3.jpg
 numlockx off
-setxkbmap de koy && xkbcomp -w 0 ~/bin/vou-tab.xkb $DISPLAY
+setxkbmap de koy && xkbcomp -w 0 ~/bin/vou-tabwin.xkb $DISPLAY
 xset r rate 300 50
 #xinput set-prop 'ETPS/2 Elantech Touchpad' 'libinput Natural Scrolling Enabled' 1
 sxhkd &
