820cbc7db2c8e2a02a3b19e9504366de1e8fb7d2
Max Schillinger <maxschillinger@web.de>
2020-08-12 23:31:50 +0200

    fish: fix delete key in st terminal

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -57,3 +57,30 @@ if type -q fzf
     # set color scheme
     set -x FZF_DEFAULT_OPTS "--color fg:-1,bg:-1,hl:#fa9a2d,fg+:3,hl+:#fa9a2d,info:150,prompt:110,spinner:150,pointer:167,marker:174 $FZF_DEFAULT_OPTS"
 end
+
+# fix delete key in st terminal
+if status is-interactive
+    switch $TERM
+
+        # Fix DEL key in st
+        case 'st*'
+            set -gx is_simple_terminal 1
+
+        case "linux"
+            set -ex is_simple_terminal
+            function fish_prompt
+                fish_fallback_prompt
+            end
+    end
+
+    if set -q is_simple_terminal
+        tput smkx ^/dev/null
+        function fish_enable_keypad_transmit --on-event fish_postexec
+            tput smkx ^/dev/null
+        end
+
+        function fish_disable_keypad_transmit --on-event fish_preexec
+            tput rmkx ^/dev/null
+        end
+    end
+end
