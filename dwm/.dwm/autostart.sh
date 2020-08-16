b273b277b1d2b4b600661254d3c1292e9f29e674
Max Schillinger <maxschillinger@web.de>
2020-08-16 18:29:16 +0200

    dwm: start compton if available

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -5,6 +5,6 @@ xset r rate 300 50
 #xinput set-prop 'ETPS/2 Elantech Touchpad' 'libinput Natural Scrolling Enabled' 1
 sxhkd &
 dunst &
-test -f ~/.screenlayout/ext-acer-monitor.sh && ~/.screenlayout/ext-acer-monitor.sh
-#st -e fish &
+test -f ~/.screenlayout/ext-monitor-acer.sh && ~/.screenlayout/ext-monitor-acer.sh
+type compton &> /dev/null && compton &
 
