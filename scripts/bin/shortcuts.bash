fd14cb18b3b05bce54f6fac7b3d95e9f2240de08
Max Schillinger <maxschillinger@web.de>
2020-08-22 13:56:46 +0200

    scripts: add script that lists all sxhkd shortcuts in fzf or rofi

---
+++ scripts/bin/shortcuts.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ "$1" = "rofi" ]; then
+    # Show all shortcuts in sxhkd in rofi
+    cat ~/.config/sxhkd/sxhkdrc | awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' | column -t -s $'\t' | rofi -dmenu -i -no-show-icons -width 1000
+else
+    cat ~/.config/sxhkd/sxhkdrc | awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' | column -t -s $'\t' | fzf
+fi
