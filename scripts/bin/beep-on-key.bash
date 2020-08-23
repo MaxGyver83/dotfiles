2ae84b4645b1c11f375b22e38b0202fd45c4fd20
Max Schillinger <maxschillinger@web.de>
2020-08-23 12:50:06 +0200

    scripts: beep-on-key: support backspace

--- scripts/bin/beep-on-key.bash
+++ scripts/bin/beep-on-key.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
-if [ "$1" = return ]; then
+if [ "$1" = return ] || [ "$1" = enter ]; then
     keycode=36
 elif [ "$1" = escape ]; then
     keycode=9
+elif [ "$1" = backspace ]; then
+    keycode=22
 else
-    # tab key
+    # default: tab key
     keycode=23
 fi
 xinput test 12 | while read in ; do [[ $in = "key press   $keycode" ]] && play -q -n synth 0.1 sin 880 2> /dev/null; done
