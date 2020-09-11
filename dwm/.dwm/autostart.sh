1ea5977e70b459276c2c51de4e229bc38cc59faf
Max Schillinger <maxschillinger@web.de>
2020-09-11 13:03:41 +0200

    dwm: use wider VOU layout; activate WXHD screen only (if available); load random wallpaper on startup

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -1,13 +1,15 @@
-test -d /home/max && feh --bg-fill ~/Bilder/Wallpaper/SeaSunset.jpg || feh --bg-fill ~/Pictures/Leo3.jpg
 numlockx off
-setxkbmap de koy && xkbcomp -w 0 ~/bin/vou-tabwin.xkb $DISPLAY
+setxkbmap de koy && xkbcomp -w 0 ~/bin/vou-wide-tabwin.xkb $DISPLAY
 xset r rate 300 50
 #xinput set-prop 'ETPS/2 Elantech Touchpad' 'libinput Natural Scrolling Enabled' 1
-sxhkd &
 dunst &
-test -f ~/.screenlayout/ext-monitor-acer.sh && ~/.screenlayout/ext-monitor-acer.sh
+xrandr | grep -q 2560x1440 && test -f ~/.screenlayout/only-peaq.sh && ~/.screenlayout/only-peaq.sh
+# test -f ~/.screenlayout/ext-monitor-acer.sh && ~/.screenlayout/ext-monitor-acer.sh
+test -d /home/max && ~/bin/set-random-wallpaper.bash || feh --bg-fill ~/Pictures/Leo3.jpg
 type compton &> /dev/null && compton &
 # trayer --widthtype request --align right &
 nm-applet &
 pasystray &
 copyq &
+sxhkd &
+~/bin/beep-on-key.bash enter &
