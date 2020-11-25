e1ef181459d90ddab34d9b85e6661e632bead58b
Max Schillinger <maxschillinger@web.de>
2020-11-25 21:56:32 +0100

    dwm: update autostart script

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -3,11 +3,12 @@ numlockx off
 #~/install/kmonad ~/.config/kmonad/vou4.kbd &
 #test -e /dev/input/by-id/usb-Telink_Wireless_Receiver-if01-event-kbd && ~/install/kmonad ~/.config/kmonad/vou4-jellycomb.kbd &
 #test -e /dev/input/by-id/usb-You_idobo_0-event-kbd && ~/install/kmonad ~/.config/kmonad/vou2-idobo.kbd &
-fish -c k &
+~/bin/start-kmonad-for-all-keyboards.fish de &
 xset r rate 300 50
 #xinput set-prop 'ETPS/2 Elantech Touchpad' 'libinput Natural Scrolling Enabled' 1
 dunst &
 xrandr | grep -q 2560x1440 && test -f ~/.screenlayout/only-peaq.sh && ~/.screenlayout/only-peaq.sh
+xrandr --output eDP-1 --brightness 0.7
 # test -f ~/.screenlayout/ext-monitor-acer.sh && ~/.screenlayout/ext-monitor-acer.sh
 test -d /home/max && ~/bin/set-random-wallpaper.bash || feh --bg-fill ~/Pictures/Leo3.jpg
 type compton &> /dev/null && compton &
@@ -19,3 +20,5 @@ sxhkd &
 # ~/bin/beep-on-key.bash enter &
 eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
 export SSH_AUTH_SOCK
+~/install/alacritty-0.6 --config-file ~/.config/alacritty/alacritty_dwm_0.6.yml -e byobu &
+firefox && sleep 1 && wmctrl -a byobu &
