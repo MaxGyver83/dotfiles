9271d4a90a45768c5576343a3cc81d6041a965fd
Maximilian Schillinger <maximilian.schillinger@daimler.com>
2020-11-30 15:30:05 +0100

    dwm: set brightness for first connected monitor; update wallpaper; use alacritty 0.6

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -8,9 +8,9 @@ xset r rate 300 50
 #xinput set-prop 'ETPS/2 Elantech Touchpad' 'libinput Natural Scrolling Enabled' 1
 dunst &
 xrandr | grep -q 2560x1440 && test -f ~/.screenlayout/only-peaq.sh && ~/.screenlayout/only-peaq.sh
-xrandr --output eDP-1 --brightness 0.7
+xrandr --output $(xrandr | grep -w connected | head -n 1 | cut -d " " -f1) --brightness 0.7
 # test -f ~/.screenlayout/ext-monitor-acer.sh && ~/.screenlayout/ext-monitor-acer.sh
-test -d /home/max && ~/bin/set-random-wallpaper.bash || feh --bg-fill ~/Pictures/Leo3.jpg
+test -d /home/max && ~/bin/set-random-wallpaper.bash || feh --bg-fill ~/Pictures/Leo4.jpg
 type compton &> /dev/null && compton &
 # trayer --widthtype request --align right &
 nm-applet &
@@ -20,5 +20,6 @@ sxhkd &
 # ~/bin/beep-on-key.bash enter &
 eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
 export SSH_AUTH_SOCK
+#alacritty --config-file ~/.config/alacritty/alacritty_dwm.yml -e byobu &
 ~/install/alacritty-0.6 --config-file ~/.config/alacritty/alacritty_dwm_0.6.yml -e byobu &
 firefox && sleep 1 && wmctrl -a byobu &
