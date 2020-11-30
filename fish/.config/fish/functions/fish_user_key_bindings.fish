ea4dd71dd6616d9cc6a409194f0104458df2ed1b
Max Schillinger <maxschillinger@web.de>
2020-11-30 20:23:43 +0100

    fish: comment out keybindings C-p, C-s and C-h (use original Alt keybinding again thanks to KMonad)

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,18 +1,19 @@
 function fish_user_key_bindings
     # Ctrl-w: delete bigword (p.e. whole path with Ctrl-w, as in bash)
     bind \cw backward-kill-bigword
-    # Ctrl-f completes only next word of autosuggestion
-    #bind \cf forward-word
+
     # jump bigword with Ctrl+b/f
     bind \cb backward-bigword
     bind \cf forward-bigword
+
     # jump bigword with Shift+LeftArrow/RightArrow
     #bind '[1;2D' backward-bigword
     #bind '[1;2C' forward-bigword
+
     bind \cq delete-or-exit
-    bind \cp __fish_paginate
-    bind \cs __fish_prepend_sudo
-    bind \ch history-token-search-backward
+    # bind \cp __fish_paginate
+    # bind \cs __fish_prepend_sudo
+    # bind \ch history-token-search-backward
 
     if type -q fzf_key_bindings
         fzf_key_bindings
