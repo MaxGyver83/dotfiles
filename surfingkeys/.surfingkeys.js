4cbe069ccbb023d42a6a46a87a2770cd1a77f393
Max Schillinger <maxschillinger@web.de>
2020-12-31 21:16:53 +0100

    surfingkeys: unmap 't'

--- surfingkeys/.surfingkeys.js
+++ surfingkeys/.surfingkeys.js
@@ -5,7 +5,7 @@ Hints.characters = 'caeiybtrnsoughzx,dwm';
 settings.hintAlign = "right";
 
 settings.blacklistPattern = /groups.google.com/i;
-unmapAllExcept(['f', 'af', 'gf','q', 'i', 'H', 't', 'S', 'D', 'E', 'R', 'e', 'd', ';e', '<Ctrl-6>', '?']);
+unmapAllExcept(['f', 'af', 'gf','q', 'i', 'H', 'S', 'D', 'E', 'R', 'e', 'd', ';e', '<Ctrl-6>', '?']);
 //unmap('r'); // Ctrl-r / F5
 //unmap('x'); // Ctrl-w
 
