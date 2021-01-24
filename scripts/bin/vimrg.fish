f9bc4c59cb93e05bda08c4e3846a743ca975ac85
Max Schillinger <maxschillinger@web.de>
2021-01-24 22:42:34 +0100

    scripts: vimrg.fish: use vim (with workaround for E994

--- scripts/bin/vimrg.fish
+++ scripts/bin/vimrg.fish
@@ -1,13 +1,14 @@
 #!/usr/bin/env fish
+
+# When used with vim: E994. Workaround: call timer_start ...
+# Save command in command history: needs autocommand in .vimrc. Does not work in nvim.
 set last_command (string split --max 1 ' ' $history[2])
 set cmd $last_command[1]
-set args $last_command[2]
-if [ $cmd != 'rg' ]
-    echo "The last command did not start with 'rg'!"
-    echo ---$cmd---
-    echo ---$last_command---
-    exit 1
+if [ $cmd = 'rg' ]
+    set args_stripped_quotes (string replace -a "'" "" $last_command[2])
+    # nvim -c "RgRaw $args_stripped_quotes"
+    vim -c "call timer_start(0, {-> execute('RgRaw $args_stripped_quotes')})"
+else
+    # nvim -c Rg
+    vim -c "call timer_start(0, {-> execute('Rg')})"
 end
-
-echo nvim -c "execute 'RgRaw $args'"
-nvim -c "execute 'RgRaw "(string replace -a "'" "" $args)"'"
