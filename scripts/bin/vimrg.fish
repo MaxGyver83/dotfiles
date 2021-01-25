f4e3412f9c94038fb7a24f5ddff7fd880b2301e3
Max Schillinger <maxschillinger@web.de>
2021-01-25 21:28:29 +0100

    scripts: vimrg.fish: use nvim (vim workaround does not work as expected)

--- scripts/bin/vimrg.fish
+++ scripts/bin/vimrg.fish
@@ -1,14 +1,15 @@
 #!/usr/bin/env fish
 
-# When used with vim: E994. Workaround: call timer_start ...
+# When used with vim -> E994. Workaround: call timer_start ...
+# But then selection of a match with "Enter" does not work.
 # Save command in command history: needs autocommand in .vimrc. Does not work in nvim.
 set last_command (string split --max 1 ' ' $history[2])
 set cmd $last_command[1]
 if [ $cmd = 'rg' ]
     set args_stripped_quotes (string replace -a "'" "" $last_command[2])
-    # nvim -c "RgRaw $args_stripped_quotes"
-    vim -c "call timer_start(0, {-> execute('RgRaw $args_stripped_quotes')})"
+    nvim -c "RgRaw $args_stripped_quotes"
+    # vim -c "call timer_start(0, {-> execute('RgRaw $args_stripped_quotes')})"
 else
-    # nvim -c Rg
-    vim -c "call timer_start(0, {-> execute('Rg')})"
+    nvim -c Rg
+    # vim -c "call timer_start(0, {-> execute('Rg')})"
 end
