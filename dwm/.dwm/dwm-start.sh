a252aa43622c54f2af448e920553bbd3f9f092bb
Maximilian Schillinger <maximilian.schillinger@daimler.com>
2021-01-26 20:44:31 +0100

    dwm: show brightness and battery level in status only when below 100%

--- dwm/.dwm/dwm-start.sh
+++ dwm/.dwm/dwm-start.sh
@@ -3,13 +3,15 @@
 # dwm statusbar - show current date and time
 while true; do
     notmuch help &> /dev/null && mailinfo="✉$(notmuch search tag:unread | wc -l), " || mailinfo=""
-    brightness=☀$(echo $(xrandr --verbose | awk '/Brightness/ { print $2; exit }')'*100' | bc | sed 's/\.00\?//')"%, "
-    battery=⎓$(cat /sys/class/power_supply/BAT0/capacity)"%, "
+    brightness_percent=$(echo $(xrandr --verbose | awk '/Brightness/ { print $2; exit }')'*100' | bc | sed 's/\.00\?//')
+    [ $brightness_percent = 100 ] && brightness="" || brightness=☀"${brightness_percent}%, "
+    battery_charge_level=$(cat /sys/class/power_supply/BAT0/capacity)
+    [ $battery_charge_level = 100 ] && battery="" || battery=⎓"${battery_charge_level}%, "
     dat=$(date "+%a %F %R")
     xsetroot -name "${mailinfo}${brightness}${battery}${dat}"
     seconds=$(date +%S)
     seconds_to_full_minute=$(( 60 - $seconds ))
-    # echo "Sleeping for $seconds_to_full_minute seconds." >> ~/.dwm.log
+    # echo "$(date): Sleeping for $seconds_to_full_minute seconds." >> ~/.dwm.sleeping.log
     sleep $seconds_to_full_minute
 done &
 
