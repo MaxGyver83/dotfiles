a817acd963bcc674d215022429a2083bdc1f4651
Maximilian Schillinger <maximilian.schillinger@daimler.com>
2021-02-01 19:53:15 +0100

    scripts: add run_forever.fish for running KMonad in an endless loop

---
+++ scripts/bin/run_forever.fish
@@ -0,0 +1,10 @@
+#!/usr/bin/fish
+set counter 0
+while true
+    set counter (math $counter + 1)
+    $argv[1] $argv[2] 2>> ~/.kmonad.loop.log
+    set message (date -Iseconds)": Restarting KMonad ($counter)"
+    echo -e \n$message >> ~/.kmonad.loop.log
+    notify-send -u low --hint int:transient:1 $message
+    sleep 1s
+end
