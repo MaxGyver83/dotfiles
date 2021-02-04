9731db118f0fe6250491bfc325c88f9541949400
Maximilian Schillinger <maximilian.schillinger@daimler.com>
2021-02-04 15:05:46 +0100

    dwm: start blueman-manager instead of blueman-tray; try Peaq monitor config over USB first; use HOME variable

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -12,7 +12,7 @@ function run {
 xset r rate 300 50
 #xinput set-prop 'ETPS/2 Elantech Touchpad' 'libinput Natural Scrolling Enabled' 1
 run dunst
-xrandr | grep -q 2560x1440 && ~/.screenlayout/peaq.sh || ~/.screenlayout/peaq-usb.sh
+xrandr | grep -q 2560x1440 && ~/.screenlayout/peaq-usb.sh || ~/.screenlayout/peaq.sh
 # [ $(xrandr | grep -wc connected) -eq 3 ] && ~/.screenlayout/2-externe-acer.sh
 xrandr --output $(xrandr | grep -w connected | head -n 1 | cut -d " " -f1) --brightness 0.7
 # test -f ~/.screenlayout/ext-monitor-acer.sh && ~/.screenlayout/ext-monitor-acer.sh
@@ -21,7 +21,7 @@ run compton
 # trayer --widthtype request --align right &
 run nm-applet
 run pasystray
-run blueman-tray
+run blueman-manager
 run copyq
 run sxhkd
 run xbanish -i shift -i control -i mod1 -i mod4
@@ -30,8 +30,8 @@ eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
 export SSH_AUTH_SOCK
 
 # start Alacritty with byobu and Firefox if not yet running
-if ! pgrep -f '/lhome/schimax/install/alacritty --config-file /lhome/schimax/.config/alacritty/alacritty_dwm_0.6.yml -e byobu' ; then
-  run ~/install/alacritty --config-file ~/.config/alacritty/alacritty_dwm_0.6.yml -e byobu
+if ! pgrep -f "$HOME/install/alacritty -e byobu" ; then
+  run ~/install/alacritty -e byobu
 fi
 if ! pgrep -f firefox ; then
   firefox &
