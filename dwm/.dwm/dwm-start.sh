79646ba589ac65e198399516a5a2439900d812ca
Max Schillinger <maxschillinger@web.de>
2021-03-08 20:02:54 +0100

    dwm: set status in update-status.bash; add network information (tunnel+Kerberos certificate)

--- dwm/.dwm/dwm-start.sh
+++ dwm/.dwm/dwm-start.sh
@@ -2,13 +2,8 @@
 
 # dwm statusbar - show current date and time
 while true; do
-    notmuch help &> /dev/null && mailinfo="✉$(notmuch search tag:unread | wc -l), " || mailinfo=""
-    brightness_percent=$(echo $(xrandr --verbose | awk '/Brightness/ { print $2; exit }')'*100' | bc | sed 's/\.00\?//')
-    [ $brightness_percent = 100 ] && brightness="" || brightness=☀"${brightness_percent}%, "
-    battery_charge_level=$(cat /sys/class/power_supply/BAT0/capacity)
-    [ $battery_charge_level = 100 ] && battery="" || battery=⎓"${battery_charge_level}%, "
-    dat=$(date "+%a %F %R")
-    xsetroot -name "${mailinfo}${brightness}${battery}${dat}"
+    ~/.dwm/update-status.bash
+
     seconds=$(date +%S)
     seconds_to_full_minute=$(( 60 - $seconds ))
     # echo "$dat: Sleeping for $seconds_to_full_minute seconds." >> ~/.dwm.sleeping.log
--- dwm/.dwm/update-status.bash
+++ dwm/.dwm/update-status.bash
@@ -1,12 +1,17 @@
 #!/bin/bash
 notmuch help &> /dev/null && mailinfo="✉$(notmuch search tag:unread | wc -l), " || mailinfo=""
 
+[ "$USER" = schimax ] && network=$(~/bin/check_network.bash)", " || network=""
+
 monitor_xpos=$(xdotool getactivewindow getwindowgeometry | grep Position | grep -o '[0-9]*,' | sed 's/,//')
 active_monitor=$(xrandr | grep -w connected | grep "+$monitor_xpos+" | head -n 1 | cut -d " " -f1)
-brightness=☀$(echo $(xrandr --verbose | grep "^$active_monitor" -A 5 | awk '/Brightness/ { print $2; exit }')'*100' | bc | sed 's/\.00\?//')"%, "
+brightness_percent=$(echo $(xrandr --verbose | grep "^$active_monitor" -A 5 | awk '/Brightness/ { print $2; exit }')'*100' | bc | sed 's/\.00\?//')
+[ $brightness_percent = 100 ] && brightness="" || brightness=☀"${brightness_percent}%, "
 
-battery=⎓$(cat /sys/class/power_supply/BAT0/capacity)"%, "
+battery_charge_level=$(cat /sys/class/power_supply/BAT0/capacity)
+[ $battery_charge_level = 100 ] && battery="" || battery=⎓"${battery_charge_level}%, "
 
 dat=$(date "+%a %F %R")
 
-xsetroot -name "${mailinfo}${brightness}${battery}${dat}"
+
+xsetroot -name "${mailinfo}${network}${brightness}${battery}${dat}"
