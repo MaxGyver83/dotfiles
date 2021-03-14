b1432939c20a5ffd0713ed09b3afe53c8c9a2ec5
Max Schillinger <maxschillinger@web.de>
2021-03-14 14:39:56 +0100

    scripts: set-random-wallpaper: redirect output if directory does not exist

--- scripts/bin/set-random-wallpaper.bash
+++ scripts/bin/set-random-wallpaper.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd ~/repos/wallpapers || cd ~/Bilder/Wallpaper || exit 1
+cd ~/repos/wallpapers > /dev/null 2>&1 || cd ~/Bilder/Wallpaper || exit 1
 feh --bg-fill $(fd '.jpg' | sort -R | tail -1)
