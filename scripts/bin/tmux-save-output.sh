f36aa0ec3e91e2f3e59e6abeb1dc8b369b91a2d6
Max Schillinger <maxschillinger@web.de>
2021-03-24 23:17:18 +0100

    scripts: tmux-save-output: keep commands (only remove prompt), remove trailing ^C, delete adjacent duplicate lines

--- scripts/bin/tmux-save-output.sh
+++ scripts/bin/tmux-save-output.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 printf 0 > /tmp/line_number
-# skip empty lines, trim lines, skip prompt lines, skip virtual env lines, keep only file names in ll output:
+# skip empty lines, trim lines, remove prompts, skip virtual env lines, keep only file names in ll output, remove ./ and ../, delete trailing ^C, delete adjacent duplicate lines:
 tmux capture-pane -p \
   | awk /./ \
   | awk '{$1=$1;print}' \
-  | sed /▶/d \
-  | sed /\(ansi/d \
+  | sed -E 's/^.*?▶ *//' \
+  | awk /./ \
+  | sed /^\(ansi/d \
   | sed -E 's/^[dl-][r-][w-][x-][^[:space:]]+ ([^[:space:]]+ ){7}//' \
   | sed -E '/^\.\.?\/?/d' \
+  | sed -E 's/\^C$//' \
+  | uniq \
   > /tmp/tmux_buffer
