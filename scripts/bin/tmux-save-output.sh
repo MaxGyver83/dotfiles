2b04690e419b7669a0c43adc309339a35da96022
Max Schillinger <maxschillinger@web.de>
2021-03-24 22:55:50 +0100

    scripts: tmux-save-output: keep only file names in ll output; skip '.[.][/]'

--- scripts/bin/tmux-save-output.sh
+++ scripts/bin/tmux-save-output.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 printf 0 > /tmp/line_number
-# skip empty lines, trim lines, skip prompt lines, skip virtual env lines:
-tmux capture-pane -p | awk /./ | awk '{$1=$1;print}' | sed /▶/d | sed /\(ansi/d > /tmp/tmux_buffer
+# skip empty lines, trim lines, skip prompt lines, skip virtual env lines, keep only file names in ll output:
+tmux capture-pane -p \
+  | awk /./ \
+  | awk '{$1=$1;print}' \
+  | sed /▶/d \
+  | sed /\(ansi/d \
+  | sed -E 's/^[dl-][r-][w-][x-][^[:space:]]+ ([^[:space:]]+ ){7}//' \
+  | sed -E '/^\.\.?\/?/d' \
+  > /tmp/tmux_buffer
