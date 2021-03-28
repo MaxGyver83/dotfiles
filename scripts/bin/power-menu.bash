a6fc817e3897fc76bd6b114a4ce01de65adfeeb0
Max Schillinger <maxschillinger@web.de>
2021-03-28 15:31:58 +0200

    scripts: add power-menu.bash

---
+++ scripts/bin/power-menu.bash
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# when called with option -w, run this script in a new alacritty window
+if [ $# -ge 1 ] && [ "$1" = "-w" ]; then
+  pid=$(pgrep -f "alacritty -t 'Power menu'")
+  test -z $pid && WINIT_X11_SCALE_FACTOR=1.0 alacritty -t "Power menu" -o "window.dimensions.columns=30" -o "window.dimensions.lines=8" -e "$0" || kill $pid
+  exit 0
+fi
+
+options='
+reboot
+shutdown
+lock screen
+kill dwm (=restart)
+exit dwm (logout)
+'
+
+# print first letter in red at the beginning of each line
+bold=$(tput bold)
+red=$(tput setaf 1)
+normal=$(tput sgr0)
+options=$(echo "$options" | sed -e "s/\(^.\)/${red}${bold}\1${normal} \1/")
+
+echo -e "Select an option:\n$options"
+read -rsn1 key
+
+case $key in
+  r) systemctl reboot ;;
+  s) systemctl poweroff ;;
+  l) png="$(xdg-user-dir PICTURES)/Leo4.png" && test -f "$png" && i3lock -n -e -f -i "$png" -t || i3lock -n -e -f -c 333333 ;;
+  q) pkill -f '^dwm' ;;
+  k) pkill dwm ;;
+  *) echo Canceled. ;;
+esac
