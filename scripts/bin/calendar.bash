89d18d979ed844410616987a1259b4f7b07de1ba
Max Schillinger <maxschillinger@web.de>
2021-04-23 17:17:32 +0200

    scripts: calendar.bash: close calendar on any keypress

--- scripts/bin/calendar.bash
+++ scripts/bin/calendar.bash
@@ -2,4 +2,4 @@
 # pid=$(pgrep -f "alacritty -d 82 9 -t Calendar -e bash -c ncal")
 # test -z $pid && alacritty -d 82 9 -t Calendar -e bash -c 'ncal -bwM3 && printf "Press Enter for closing " && read' || kill $pid
 pid=$(pgrep -f "alacritty -t Calendar")
-test -z $pid && WINIT_X11_SCALE_FACTOR=1.0 alacritty -t Calendar -o "window.dimensions.columns=82" -o "window.dimensions.lines=9" -e bash -c 'ncal -bwM3 && printf "Press Enter for closing " && read' || kill $pid
+test -z $pid && WINIT_X11_SCALE_FACTOR=1.0 alacritty -t Calendar -o "window.dimensions.columns=82" -o "window.dimensions.lines=9" -e bash -c 'ncal -bwM3 && printf "Press any key for closing " && read -rsn1' || kill $pid
