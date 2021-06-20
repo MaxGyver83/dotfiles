63f5a121b5b82ac04eec7a0ff5e10534fa8e83b7
Max Schillinger <maxschillinger@web.de>
2021-06-21 00:00:22 +0200

    dwm: make autostart.sh work with bspwm, too

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -8,24 +8,28 @@ function run {
   fi
 }
 
+wmctrl -m | grep -q bspwm && WM=bspwm
+
 # numlockx off
 ~/bin/start-kmonad.fish --keyboard all &
 #xset r rate 300 50
 #xinput set-prop 'ETPS/2 Elantech Touchpad' 'libinput Natural Scrolling Enabled' 1
 run dunst
 xrandr | grep -q 2560x1440 && ~/.screenlayout/peaq-usb.sh || ~/.screenlayout/peaq.sh
-# [ $(xrandr | grep -wc connected) -eq 3 ] && ~/.screenlayout/2-externe-acer.sh
+[ $(xrandr | grep -wc connected) -eq 3 ] && ~/.screenlayout/2-externe-acer.sh
 xrandr --output $(xrandr | grep -w connected | head -n 1 | cut -d " " -f1) --brightness 0.7
 # test -f ~/.screenlayout/ext-monitor-acer.sh && ~/.screenlayout/ext-monitor-acer.sh
 ~/bin/set-wallpaper.bash
+# run trayer only in bspwm
+[ "$WM" = bspwm ] && run trayer --edge top --align right --margin 220 --widthtype request --height 28 --tint 0x292b2e --transparent true --expand true --SetDockType true --alpha 0
 # compton: `--focus...` is needed to not dim the status bar and rofi
 pgrep -f compton > /dev/null 2>&1 || compton --inactive-dim 0.2 --focus-exclude 'x = 0 && y = 0 && override_redirect = true || class_g = "Rofi" || class_g = "Firefox" && argb' &
-# trayer --widthtype request --align right &
 run nm-applet
 run pasystray
 run blueman-applet
 run copyq
-run sxhkd
+# start sxhkd if not running bspwm
+[ "$WM" = bspwm ] || run sxhkd
 run xbanish -i shift -i control -i mod1 -i mod4
 # ~/bin/beep-on-key.bash enter &
 eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
