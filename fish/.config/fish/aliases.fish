24a1189ac9894b4f5c818157a040c5f01c0e73cf
Max Schillinger <maxschillinger@web.de>
2021-06-21 00:07:22 +0200

    fish: alias 'ti': set VOU after closing tipp10

--- fish/.config/fish/aliases.fish
+++ fish/.config/fish/aliases.fish
@@ -92,7 +92,7 @@ abbr s 'xrandr | grep -q 2560x1440 && ~/.screenlayout/peaq-usb.sh || ~/.screenla
 # redshift: red=night mode (darker, redish), notred=day mode
 abbr red 'redshift -P -O 3500 -b 0.7 || redshift -O 3500 -b 0.7'
 abbr notred 'redshift -P -O 6500 || redshift -O 6500'
-abbr ti 'pkill run_forever ; pkill kmonad ; setxkbmap de ; tipp10'
+abbr ti 'pkill run_forever ; pkill kmonad ; setxkbmap de ; tipp10 ; ~/bin/start-kmonad.fish -k all'
 
 alias akku='upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "power|updated|state|energy|time to|perc|capa" | grep -v design'
 
