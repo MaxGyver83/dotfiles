41328214faa1b33ec754b1c94e68507a1cde3307
Max Schillinger <maxschillinger@web.de>
2021-06-23 18:41:33 +0200

    Add xrandr screenlayouts for laptop and/or Peaq display

---
+++ xrandr/.screenlayout/peaq.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+if xrandr | grep -w connected | grep -q '^DP-1-1 ' ; then
+  xrandr --output HDMI-1-2 --off \
+         --output eDP-1-1 --off \
+         --output HDMI-1-1 --off \
+         --output DP-1-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
+         --output DP-1-2 --off
+
+elif xrandr | grep -w connected | grep -q '^HDMI-1-1 ' ; then
+  xrandr --output HDMI-1-2 --off \
+         --output eDP-1-1 --off \
+         --output HDMI-1-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
+         --output DP-1-1 --off \
+         --output DP-1-2 --off
+
+elif xrandr | grep -w connected | grep -q '^DP-1-1-1 ' ; then
+  xrandr --output eDP-1-1 --off \
+         --output DP-1-1-3 --off \
+         --output DP-1-1-2 --off \
+         --output DP-1-1-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
+         --output DP-1-3 --off \
+         --output DP-1-2 --off \
+         --output DP-1-1 --off
+else
+  # laptop only
+  xrandr --output HDMI-1-2 --off \
+         --output HDMI-1-1 --off \
+         --output eDP-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+         --output DP-1-1-3 --off \
+         --output DP-1-1-2 --off \
+         --output DP-1-1-1 --off \
+         --output DP-1-2 --off \
+         --output DP-1-1 --off
+fi
