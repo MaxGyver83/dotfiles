d3a8a3cfa6e86ff6f60f3dfe49853d763b2c7c65
Max Schillinger <maxschillinger@web.de>
2021-07-06 20:14:20 +0200

    fish: add function 'e': edit last command line output in vim

---
+++ fish/.config/fish/functions/e.fish
@@ -0,0 +1,6 @@
+function e --description 'Copy n-th line to last into command line and prepend "vim "'
+    if not count $argv > /dev/null
+        set argv 1
+    end
+    tmux capture-pane -p | awk /./ | sed /▶/d | sed /\(ansi/d| tail -$argv | head -1 | tr "\n" " " | string trim | tmux load-buffer - && tmux send "vim " && tmux paste-buffer -s " "
+end
