e4e5dd51428e602418340c87333d2f6d2892021c
Max Schillinger <maxschillinger@web.de>
2021-09-12 21:40:35 +0200

    scripts: cheatsheet.bash: skip alacritty config

--- scripts/bin/cheatsheet.bash
+++ scripts/bin/cheatsheet.bash
@@ -5,7 +5,7 @@ if [ -z "$pid" ] ; then
   rows=$((rows + 1))
   cols=$(wc -L ~/bin/cheatsheet.txt | cut -d' ' -f1)
   # ~/install/alacritty-0.6 --config-file ~/.config/alacritty/alacritty_dwm_0.6.yml -t Cheatsheet -d $cols $rows -e bash -c 'cat ~/bin/cheatsheet.txt && read' &
-  WINIT_X11_SCALE_FACTOR=1.0 ~/install/alacritty-0.7.1 -t Cheatsheet -o "window.dimensions.columns=$cols" -o "window.dimensions.lines=$rows" -e bash -c 'cat ~/bin/cheatsheet.txt && read -rsn1'
+  WINIT_X11_SCALE_FACTOR=1.0 ~/install/alacritty -t Cheatsheet -o "window.dimensions.columns=$cols" -o "window.dimensions.lines=$rows" -e bash -c 'cat ~/bin/cheatsheet.txt && read -rsn1'
 else
   kill $pid
 fi
