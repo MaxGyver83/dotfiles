bb683024bcb55373bc8e34abfa6ba1b92d20b835
Max Schillinger <maxschillinger@web.de>
2021-10-12 19:30:02 +0200

    fish: o.fish: ignore lines starting with '(shm'

--- fish/.config/fish/functions/o.fish
+++ fish/.config/fish/functions/o.fish
@@ -2,5 +2,5 @@ function o --description 'Copy n-th line to last into command line'
     if not count $argv > /dev/null
         set argv 1
     end
-    tmux capture-pane -p | awk /./ | sed /▶/d | sed /\(ansi/d | sed /\(pytest/d | tail -$argv | head -1 | tr "\n" " " | string trim | tmux load-buffer - && tmux paste-buffer -s " "
+    tmux capture-pane -p | awk /./ | sed /▶/d | sed /\(ansi/d | sed /\(pytest/d | sed /\(shm/d | tail -$argv | head -1 | tr "\n" " " | string trim | tmux load-buffer - && tmux paste-buffer -s " "
 end
