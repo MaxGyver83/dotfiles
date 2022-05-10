852a6d335a23bec09bdc7b20a86547e405ab956c
Max Schillinger <maxschillinger@web.de>
2022-05-10 11:20:46 +0200

    fish: Reformat and update o.fish and e.fish

--- fish/.config/fish/functions/o.fish
+++ fish/.config/fish/functions/o.fish
@@ -2,5 +2,16 @@ function o --description 'Copy n-th line to last into command line'
     if not count $argv > /dev/null
         set argv 1
     end
-    tmux capture-pane -p | awk /./ | sed /▶/d | sed /\(ansi/d | sed /\(pytest/d | sed /\(shm/d | tail -$argv | head -1 | tr "\n" " " | string trim | tmux load-buffer - && tmux paste-buffer -s " "
+    tmux capture-pane -p \
+        | awk /./ \
+        | sed /▶/d \
+        | sed /\(ansi/d \
+        | sed /\(pytest/d \
+        | sed /\(dt_/d \
+        | tail -$argv \
+        | head -1 \
+        | tr "\n" " " \
+        | string trim \
+        | tmux load-buffer - \
+        && tmux paste-buffer -s " "
 end
