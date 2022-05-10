3a6168a306a47e939fcdf60bcb4e40fe0183dada
Max Schillinger <maxschillinger@web.de>
2022-05-10 11:20:46 +0200

    scripts: snippets.sh: Trim trailing newline; allow \n for multiline strings; paste to primary selection AND clipboard (because of Firefox)

--- scripts/bin/snippets.sh
+++ scripts/bin/snippets.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-selection=$(rofi -i -dmenu $@ < ~/.snippets)
-echo -n "$selection" | xclip
+# select line from ~/.snippets with rofi, replace '\n's by newlines, trim the final newline,
+# and write to both primary selection and clipboard (because Firefox pastes the clipboard on Shift-Insert)
+rofi -i -dmenu $@ < ~/.snippets | sed 's/\\n/\n/g' | head -c -1 | xclip -i -sel p -f | xclip -i -sel c
 sleep 0.1
 xdotool key shift+Insert
