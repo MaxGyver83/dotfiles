2e3b46c0fdc235fd686bdadc30d1a5b82d3f8483
Max Schillinger <maxschillinger@web.de>
2022-10-06 19:18:17 +0200

    dwm: update-status: Show when charging; show headset profile

--- dwm/.dwm/update-status.bash
+++ dwm/.dwm/update-status.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
 notmuch help &> /dev/null && mailinfo="✉$(notmuch search tag:unread | wc -l), " || mailinfo=""
 
+if test -f ~/bin/toggle_bluetooth_profile_WH-XB910N.sh ; then
+  headset_profile="$(~/bin/toggle_bluetooth_profile_WH-XB910N.sh --status)"
+  [ -n "$headset_profile" ] && headset_profile="${headset_profile}, "
+fi
+
 [ "$USER" = schimax ] && network=$(~/bin/check_network.bash)", " || network=""
 
 monitor_xpos=$(xdotool getactivewindow getwindowgeometry | grep Position | grep -o '[0-9]*,' | sed 's/,//')
@@ -9,12 +14,17 @@ brightness_percent=$(echo $(xrandr --verbose | grep "^$active_monitor" -A 5 | aw
 [ $brightness_percent = 100 ] && brightness="" || brightness=☀"${brightness_percent}%, "
 
 battery=""
+if test -f /sys/class/power_supply/BAT0/status ; then
+  battery_status=$(cat /sys/class/power_supply/BAT0/status)
+  [ "$battery_status" = Charging ] && battery="⎓↑"
+fi
 if test -f /sys/class/power_supply/BAT0/capacity ; then
   battery_charge_level=$(cat /sys/class/power_supply/BAT0/capacity)
-  [ $battery_charge_level = 100 ] || battery=⎓"${battery_charge_level}%, "
+  [ $battery_charge_level = 100 ] || battery="${battery:-⎓}${battery_charge_level}%"
 fi
+[ "$battery" ] && battery="$battery, "
 
 dat=$(date "+%a %F %R")
 
 
-xsetroot -name "${mailinfo}${network}${brightness}${battery}${dat}"
+xsetroot -name "${headset_profile}${mailinfo}${network}${brightness}${battery}${dat}"
