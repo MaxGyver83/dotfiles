e5bb38e628e6b30bdf5d5b043a1a872a7ed7616f
Max Schillinger <maxschillinger@web.de>
2022-10-29 19:44:08 +0200

    dwm: autostart: Remove bspwm settings; don't reduce brightness; improve performance on personal laptop

--- dwm/.config/dwm/autostart.sh
+++ dwm/.config/dwm/autostart.sh
@@ -16,32 +16,32 @@ run() {
 # See https://unix.stackexchange.com/a/230442/305474
 export NO_AT_BRIDGE=1
 
-wmctrl -m | grep -q bspwm && WM=bspwm
-
 # numlockx off
 ~/bin/start-kmonad.fish --keyboard all &
 #xset r rate 300 50
 #xinput set-prop 'ETPS/2 Elantech Touchpad' 'libinput Natural Scrolling Enabled' 1
 run dunst
-if [ "$WM" != bspwm ]; then
+echo "$(timestamp) Switch to external screen (if available)"
+if [ "$(hostname)" = 'max-laptop' ]; then
+  [ "$(head -1 /sys/class/drm/card0-DP-1/modes)" = '2560x1440' ] && ~/.screenlayout/peaq.sh
+else
   xrandr | grep -q 2560x1440 && ~/.screenlayout/peaq.sh
   xrandr | grep -q 3840x2160 && ~/.screenlayout/dell.sh
-  [ $(xrandr | grep -wc connected) -eq 3 ] && ~/.screenlayout/2x-acer.sh
+  # [ $(xrandr | grep -wc connected) -eq 3 ] && ~/.screenlayout/2x-acer.sh
 fi
+# echo "$(timestamp) Set first (=laptop) display to 70% brightness"
 # set first (=laptop) display to 70% brightness
-xrandr --output $(xrandr | grep -w connected | head -n 1 | cut -d " " -f1) --brightness 0.7
+# xrandr --output $(xrandr | grep -w connected | head -n 1 | cut -d " " -f1) --brightness 0.7
 # test -f ~/.screenlayout/ext-monitor-acer.sh && ~/.screenlayout/ext-monitor-acer.sh
 run /bin/sh ~/bin/set-wallpaper.bash
-# run trayer only in bspwm
-[ "$WM" = bspwm ] && run trayer --edge top --align right --margin 220 --widthtype request --height 28 --tint 0x292b2e --transparent true --expand true --SetDockType true --alpha 0
 # compton: `--focus...` is needed to not dim the status bar and rofi
-pgrep -f compton > /dev/null 2>&1 || compton &
+run picom
 run nm-applet
 run pasystray
 run blueman-applet
 run copyq
-# start sxhkd if not running bspwm
-[ "$WM" = bspwm ] || run sxhkd
+run sxhkd
+run "$(\ls ~/install/Internxt-Drive-* | head -n 1)"
 # run xbanish -i shift -i control -i mod1 -i mod4
 # ~/bin/beep-on-key.bash enter &
 eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
