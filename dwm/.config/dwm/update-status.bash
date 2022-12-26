622e4b1ab914def5aa7e4fc67761bbd4b2b6ea1f
Max Schillinger <maxschillinger@web.de>
2022-12-26 21:08:59 +0100

    dwm: update-status.bash: Show hardware brightness for laptop display

--- dwm/.config/dwm/update-status.bash
+++ dwm/.config/dwm/update-status.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
 # notmuch help &> /dev/null && mailinfo="✉$(notmuch search tag:unread | wc -l), " || mailinfo=""
 
+exists() { command -v "$1" > /dev/null 2>&1 ; }
+round() { LC_ALL=C printf '%.0f' "$1"; }
+
 if test -f ~/bin/toggle_bluetooth_profile_WH-XB910N.sh ; then
   headset_profile="$(~/bin/toggle_bluetooth_profile_WH-XB910N.sh --status)"
   [ "$headset_profile" ] && headset_profile="${headset_profile}, "
@@ -10,7 +13,11 @@ fi
 
 monitor_xpos=$(xdotool getactivewindow getwindowgeometry | grep Position | grep -o '[0-9]*,' | sed 's/,//')
 active_monitor=$(xrandr | grep -w connected | grep "+$monitor_xpos+" | head -n 1 | cut -d " " -f1)
-brightness_percent=$(xrandr --verbose | grep "^$active_monitor" -A 5 | awk '/Brightness/ { printf "%.0f\n", $2 * 100; exit }')
+if [ "$(hostname)" = 'max-laptop' ] && [ "$active_monitor" = 'eDP-1' ] && exists light ; then
+  brightness_percent="$(round "$(light -G)")"
+else
+  brightness_percent=$(xrandr --verbose | grep "^$active_monitor" -A 5 | awk '/Brightness/ { printf "%.0f\n", $2 * 100; exit }')
+fi
 [ $brightness_percent = 100 ] && brightness="" || brightness=☀"${brightness_percent}%, "
 
 battery=""
