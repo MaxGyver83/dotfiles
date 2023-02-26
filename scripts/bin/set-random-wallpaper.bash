d6969074fce490291f5a26eab530754ccf3d08f7
Max Schillinger <maxschillinger@web.de>
2023-02-26 16:28:41 +0100

    scripts: set-random-wallpaper: Add possible wallpaper location

--- scripts/bin/set-random-wallpaper.bash
+++ scripts/bin/set-random-wallpaper.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd ~/repos/wallpapers > /dev/null 2>&1 || cd ~/Bilder/Wallpaper || exit 1
+cd ~/repos/wallpapers > /dev/null 2>&1 || cd ~/Bilder/Wallpaper > /dev/null 2>&1 || cd ~/Pictures/wallpapers || exit 1
 feh --bg-fill $(fd '.jpg' | sort -R | tail -1)
