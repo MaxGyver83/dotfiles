ff0f24d2294a7d1e004b24b8faf69c1c7f1ea122
Max Schillinger <maxschillinger@web.de>
2023-05-11 16:31:22 +0200

    scripts: Update tmux-select-{and-paste-,}filename.sh

--- scripts/bin/tmux-select-filename.sh
+++ scripts/bin/tmux-select-filename.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-tmux capture-pane -p -S 0 | tac | grep -oP '(~/)?[./[:alnum:]_-]*[./][./[:alnum:]_-]+|\(\K\w+(?=|)' | awk 'length($0)>5 && !x[$0]++' | fzf --no-sort
+tmux capture-pane -p -S 0 | tac | grep -oP '(~/)?[./[:alnum:]_-]*[./][./[:alnum:]_-]+' | awk 'length($0)>5 && !x[$0]++' | fzf --multi --no-sort
