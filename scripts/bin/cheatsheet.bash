c1146bc6007c5ea0dff9840d5c1e492bc6169e16
Max Schillinger <maxschillinger@web.de>
2023-06-04 20:50:07 +0200

    scripts: cheatsheet.bash: Fixed height; use pager

--- scripts/bin/cheatsheet.bash
+++ scripts/bin/cheatsheet.bash
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 pid=$(pgrep -f "alacritty.*-t Cheatsheet")
 if [ -z "$pid" ] ; then
-  rows=$(wc -l ~/bin/cheatsheet.txt | cut -d' ' -f1)
-  rows=$((rows + 1))
+  # rows=$(wc -l ~/bin/cheatsheet.txt | cut -d' ' -f1)
+  # rows=$((rows + 1))
+  rows=45
   cols=$(wc -L ~/bin/cheatsheet.txt | cut -d' ' -f1)
-  # ~/install/alacritty-0.6 --config-file ~/.config/alacritty/alacritty_dwm_0.6.yml -t Cheatsheet -d $cols $rows -e bash -c 'cat ~/bin/cheatsheet.txt && read' &
-  WINIT_X11_SCALE_FACTOR=1.0 ~/install/alacritty -t Cheatsheet -o "window.dimensions.columns=$cols" -o "window.dimensions.lines=$rows" -e bash -c 'cat ~/bin/cheatsheet.txt && read -rsn1'
+  WINIT_X11_SCALE_FACTOR=1.0 ~/install/alacritty -t Cheatsheet -o "window.dimensions.columns=$cols" -o "window.dimensions.lines=$rows" -e bash -c 'less ~/bin/cheatsheet.txt'
 else
   kill $pid
 fi
