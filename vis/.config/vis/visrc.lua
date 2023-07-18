794f78b8446f6975eab98919af3623012bd3c291
Max Schillinger <maxschillinger@web.de>
2023-07-18 22:09:18 +0200

    vis: Configure preview windows for vis-fzf-open; add more fzf keybindings

--- vis/.config/vis/visrc.lua
+++ vis/.config/vis/visrc.lua
@@ -34,6 +34,38 @@ vis.events.subscribe(vis.events.WIN_OPEN, function(win)
 	vis:command('set show-tabs on')
 end)
 
+my_fzf_args = string.gsub([[
+    --bind=$my_fzf_key_bindings \
+    --color fg:242,bg:236,hl:65,fg+:15,bg+:239,hl+:108 \
+    --color info:108,prompt:109,spinner:108,pointer:168,marker:168 \
+    --delimiter / --nth -1 \
+    --height=70% \
+    --inline-info \
+    --no-mouse \
+    --preview-window=up:70% \
+    --preview="(
+        bat --style=changes,grid,numbers --color=always {} ||
+        highlight -O ansi -l {} ||
+        coderay {} ||
+        rougify {} ||
+        cat {}
+    ) 2> /dev/null | head -1000"
+]], '%$([%w_]+)', {
+    my_fzf_key_bindings=table.concat({
+        "alt-j:preview-down",
+        "alt-k:preview-up",
+        "ctrl-f:preview-page-down",
+        "ctrl-b:preview-page-up",
+        "?:toggle-preview",
+        "alt-w:toggle-preview-wrap",
+        "ctrl-z:clear-screen"
+    }, ",")
+})
+
+local plugin_vis_open = require('plugins/vis-fzf-open')
+-- Arguments passed to fzf (default: "")
+plugin_vis_open.fzf_args = my_fzf_args
+
 vis:map(vis.modes.NORMAL, ' w', ':w<Enter>')
 vis:map(vis.modes.NORMAL, ' q', ':q<Enter>')
 vis:map(vis.modes.NORMAL, '<C-s>', ':w<Enter>')
@@ -54,6 +86,9 @@ vis:map(vis.modes.NORMAL, ' y', '"+y')
 vis:map(vis.modes.VISUAL, ' y', '"+y')
 
 vis:map(vis.modes.NORMAL, ' ff', ':fzf<Enter>')
+vis:map(vis.modes.NORMAL, ' fh', ':fzf --search-path $HOME<Enter>')
+vis:map(vis.modes.NORMAL, ' fr', ':fzf --search-path /<Enter>')
+vis:map(vis.modes.NORMAL, ' fg', ':fzf --search-path "$(git rev-parse --show-toplevel<C-s>)"<Enter>')
 
 vis:map(vis.modes.NORMAL, ' h', ':fzfmru<Enter>')
 
