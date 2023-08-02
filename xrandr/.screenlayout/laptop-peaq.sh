33c53d9809e54464b92da5694cc65be2e1a7bea9
Max Schillinger <maxschillinger@web.de>
2023-08-02 09:42:19 +0200

    xrandr: Update laptop-peaq.sh

--- xrandr/.screenlayout/laptop-peaq.sh
+++ xrandr/.screenlayout/laptop-peaq.sh
@@ -29,6 +29,15 @@ elif xrandr | grep -w connected | grep -q '^DP-1-1-1 ' ; then
          --output DP-1-2 --off \
          --output DP-1-1 --off
 
+elif xrandr | grep -w connected | grep -q '^DP-3 ' ; then
+  xrandr --output eDP-1 --mode 1920x1080 --pos 2560x168 --rotate normal \
+         --output DP-1-1-3 --off \
+         --output DP-1-1-2 --off \
+         --output DP-3 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
+         --output DP-1-3 --off \
+         --output DP-1-2 --off \
+         --output DP-1-1 --off
+
 else
   xrandr --output eDP-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
          --output HDMI-1-2 --off \
