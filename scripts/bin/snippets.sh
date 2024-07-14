c30f9ff9a625a92b097d35f3e73a42b5e9d0d288
Max Schillinger <maxschillinger@web.de>
2024-07-14 19:37:45 +0200

    scripts: snippets: Support dmenu as alternative to rofi

--- scripts/bin/snippets.sh
+++ scripts/bin/snippets.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
-# select line from ~/.snippets with rofi, replace '\n's by newlines, trim the final newline,
-# and write to both primary selection and clipboard (because Firefox pastes the clipboard on Shift-Insert)
-rofi -i -dmenu $@ < ~/.snippets | sed 's/\\n/\n/g' | head -c -1 | xclip -i -sel p -f | xclip -i -sel c
+# Execute snippets.sh for rofi or snippets-dmenu.sh (symlink) for dmenu
+if [ ${0##*/} = 'snippets-dmenu.sh' ]; then
+    # dmenu with center patch
+    cmd="$HOME/.local/bin/dmenu -i -c -l 10 -fn monospace:size=14"
+else
+    cmd='rofi -i -dmenu -kb-cancel Escape,Control+c,Control+g'
+fi
+
+# select line from ~/.snippets with rofi or dmenu, replace '\n's by newlines,
+# trim the final newline, and write to both primary selection and clipboard
+# (because Firefox pastes the clipboard on Shift-Insert)
+$cmd $@ < ~/.snippets \
+    | sed 's/\\n/\n/g' \
+    | head -c -1 \
+    | xclip -i -sel p -f \
+    | xclip -i -sel c
 sleep 0.1
 xdotool key shift+Insert
