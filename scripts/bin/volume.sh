b3c0f6cc0ac42e3f77242fa28a9dc174b324d97b
Max Schillinger <maxschillinger@web.de>
2025-06-13 11:10:34 +0200

    scripts: volume.sh: Prettier notification

--- scripts/bin/volume.sh
+++ scripts/bin/volume.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
-BRIGHT=$(stdbuf -o0 xrandr --verbose | awk '/Brightness/ { print $2; exit }')
-
-STEP=0.1
+notify() {
+    status=$(amixer sget Master | grep 'Mono:' | awk '{print $4,$6}' | tr -d '[]' | sd ' on' '' | sd 'off' '(muted)')
+    notify-send -t 500 "$status"
+}
 
 if [ "$1" = '+' ]; then
     /usr/bin/pactl set-sink-volume "@DEFAULT_SINK@" "+5%"
-    notify-send -t 500 $(amixer sget Master | grep 'Mono:' | awk '{print $4,$6}')
-
 elif [ "$1" = '-' ]; then
     /usr/bin/pactl set-sink-volume "@DEFAULT_SINK@" "-5%"
-    notify-send -t 500 $(amixer sget Master | grep 'Mono:' | awk '{print $4,$6}')
 else
     # toggle mute
     /usr/bin/pactl set-sink-mute "@DEFAULT_SINK@" toggle
-    notify-send -t 500 $(amixer sget Master | grep 'Mono:' | awk '{print $4,$6}')
 fi
 
+notify
