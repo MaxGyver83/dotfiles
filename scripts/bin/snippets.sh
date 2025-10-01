cf661b238f598c4de7be7d3d22709c476e9cf784
Max Schillinger <maxschillinger@web.de>
2025-10-01 22:27:59 +0200

    scripts: snippets.sh: Support snippet lists for `dmenu -1`

--- scripts/bin/snippets.sh
+++ scripts/bin/snippets.sh
@@ -4,7 +4,12 @@
 if [ ${0##*/} = 'snippets-dmenu.sh' ]; then
     linecount=$(cat ~/.snippets | wc -l)
     # dmenu with center patch
-    cmd="$HOME/.local/bin/dmenu -i -c -l $linecount -fn monospace:size=14"
+    cmd="dmenu -i -c -l $linecount -fn monospace:size=14"
+elif [ ${0##*/} = 'snippets-dmenu1.sh' ]; then
+    SNIPPETS=~/.snippets1
+    linecount=$(cat $SNIPPETS | wc -l)
+    # dmenu with center patch and instant_first_char patch
+    cmd="dmenu -1 -c -l $linecount -fn monospace:size=14"
 else
     cmd='rofi -i -dmenu -kb-cancel Escape,Control+c,Control+g'
 fi
@@ -12,7 +17,8 @@ fi
 # select line from ~/.snippets with rofi or dmenu, replace '\n's by newlines,
 # trim the final newline, and write to both primary selection and clipboard
 # (because Firefox pastes the clipboard on Shift-Insert)
-$cmd $@ < ~/.snippets \
+$cmd $@ < ${SNIPPETS:-~/.snippets} \
+    | sed 's/^. //' \
     | sed 's/\\n/\n/g' \
     | head -c -1 \
     | xclip -i -sel p -f \
