56d1eed7afcf235a540753bf6046ef8a7bafab44
Max Schillinger <maxschillinger@web.de>
2026-09-07 10:37:15 +0200

    surfingkeys: Unmap most keys on localhost:8090

--- surfingkeys/.surfingkeys.js
+++ surfingkeys/.surfingkeys.js
@@ -22,6 +22,7 @@ unmapAllExcept(['f', 'q', 'i', ';e'], /mail\.google\.com/i);
 unmapAllExcept(['f', 'q', 'i', ';e', 'T', '<Ctrl-6>'], /maximilian-schillinger\.de\/n\//i);
 unmapAllExcept(['f', 'q', 'i', ';e', 'T', '<Ctrl-6>'], /http:\/\/127\.0\.0\.1:8080/i);
 unmapAllExcept(['f', 'q', 'i', ';e', 'T', '<Ctrl-6>'], /localhost:8080/);
+unmapAllExcept(['f', 'q', 'i', ';e', 'T', '<Ctrl-6>'], /localhost:8090/);
 unmapAllExcept(['f', 'q', 'i', ';e', 'T', '<Ctrl-6>'], /\/Downloads\/Wiki\//);
 unmapAllExcept(['f', 'q', 'i', ';e', 'T', '<Ctrl-6>'], /TW5\/editions\/tw5\.com\/output\/index\.html/);
 //unmap('t', /http:\/\/127\.0\.0\.1:8080/i);
